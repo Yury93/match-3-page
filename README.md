5e3f3916e45b2ec1438ae370e59052954312020c
Yury93 <imezomorf93@gmail.com>
2025-06-30 08:05:13 +0300

    update config levels

--- README.md
+++
@@ -1,6 +0,0 @@
-# match-3-page   
-***Технологический стек***  
-Cocos Creator 3.x - *движок*  
-TypeScript - *язык разработки*   
-
-- *поиграть можно тут* https://yury93.github.io/match-3-page/
--- src/settings.d51a8.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game/scenes/game.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"c8b59",main:"e8ad6"}};
